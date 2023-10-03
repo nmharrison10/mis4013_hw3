30d9e335ce0e4b0bc97d080c1ec81dc778a113fd
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-03 16:54:33 -0500

    Create view-countries.php

---
+++ view-countries.php
@@ -0,0 +1,27 @@
+<h1>Countries</h1>
+<div class="table-responsive">
+  <table class="table">
+    <head>
+      <tr>
+      <th>ID</th>
+      <th>Country</th>
+      <th>Capital</th>
+      </tr>
+    </head>
+    <body>
+<?php
+while ($country = $countries->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $country['country_id']?></td>
+  <td><?php echo $country['country_name']?></td>
+  <td><?php echo $country['capital']?></td>
+</tr>
+<?php
+}
+?>
+    </body>
+  </table>
+</div>
+<?php
+?>
