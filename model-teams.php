d2b0e7667c5bb3f68c519c6e9f48e72e2343b689
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-03 20:32:54 -0500

    Create model-teams.php

---
+++ model-teams.php
@@ -0,0 +1,15 @@
+<?php
+function selectTeams() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, team_name, country_id FROM `team`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
