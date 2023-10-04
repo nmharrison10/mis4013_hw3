1aa2dbbf057b93c66470db3eda6542bbc60dffb8
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-03 22:57:02 -0500

    Create view-races.php

---
+++ view-races.php
@@ -0,0 +1,28 @@
+<h1>Formula 1 Races</h1>
+<div class="table-responsive">
+  <table class="table">
+    <head>
+      <tr>
+      <th>ID</th>
+      <th>Track</th>
+      <th>Date</th>
+      <th>Winner</th>
+        <th></th>
+      </tr>
+    </head>
+    <body>
+<?php
+while ($race = $races->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $race['race_id']?></td>
+  <td><?php echo $race['track']?></td>
+  <td><?php echo $race['date']?></td>
+  <td><?php echo $race['driver_name']?></td>
+</tr>
+<?php
+}
+?>
+    </body>
+  </table>
+</div>
