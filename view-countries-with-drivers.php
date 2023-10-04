4763fbd2da4275ef8f151d3bb0318004acccb744
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-03 22:38:10 -0500

    Update view-countries-with-drivers.php

--- view-countries-with-drivers.php
+++ view-countries-with-drivers.php
@@ -7,13 +7,16 @@ while ($country = $countries->fetch_assoc()) {
           <div class="card-body">
             <h5 class="card-title"><?php echo $country['country_name']; ?></h5>
             <p class="card-text">
+            <ul class="list-group">
 <?php
   $drivers=SelectDriversByCountry($country['country_id']);
   while($driver = $drivers->fetch_assoc()){
 ?>
+        <li class="list-group-item"><?php echo $driver['driver_id']; ?> - <?php echo $driver['driver_name']; ?> - <?php echo $driver['driver_age']; ?> - <?php echo $driver['team_name']; ?></li>
 <?php
   }
 ?>
+            </ul>
             </p>
             <p class="card-text"><small class="text-body-secondary"><td>Capital: <?php echo $country['capital']; ?></td>/small></p>
           </div>
