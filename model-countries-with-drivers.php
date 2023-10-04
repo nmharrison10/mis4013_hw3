710a59bf961e10db9b3253f04da50406d0b55fad
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-03 22:14:56 -0500

    Create model-countries-with-drivers.php

---
+++ model-countries-with-drivers.php
@@ -0,0 +1,15 @@
+<?php
+function selectCountries() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT country_id, country_name, capital FROM `country`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
