6ee8b19d7fe5297661a9f9a2caf4ef18a65cda45
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-03 22:19:52 -0500

    Update model-countries-with-drivers.php

--- model-countries-with-drivers.php
+++ model-countries-with-drivers.php
@@ -12,4 +12,18 @@ function selectCountries() {
         throw $e;
     }
 }
+function selectDriversByCountry($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT driver_id, driver_name, driver_age, team_name from driver d join country c on d.country_id=c.country_id join team t on d.team_id=t.team_id where c.country_id=?");
+        $stmt->bind_param("i", $cid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
