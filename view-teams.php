04d7e96c9c7713936a3eaf7741f10b896d48e00d
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-03 20:38:02 -0500

    Create view-teams.php

---
+++ view-teams.php
@@ -0,0 +1,27 @@
+<h1>Countries</h1>
+<div class="table-responsive">
+  <table class="table">
+    <head>
+      <tr>
+      <th>ID</th>
+      <th>Team Name</th>
+      <th>Team Principal</th>
+      </tr>
+    </head>
+    <body>
+<?php
+while ($team = $teams->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $team['team_id']?></td>
+  <td><?php echo $team['team_name']?></td>
+  <td><?php echo $team['principal']?></td>
+</tr>
+<?php
+}
+?>
+    </body>
+  </table>
+</div>
+<?php
+?>
