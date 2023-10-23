e21ae3f0ee30d5dc7d55d0ac707bcf26b5d2df7a
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-23 14:12:02 -0500

    Update model-countries.php

--- model-countries.php
+++ model-countries.php
@@ -12,4 +12,47 @@ function selectCountries() {
         throw $e;
     }
 }
+
+function insertCountry($cName, $capName) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `country` (`country_name`, `capital`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $cName, $capName);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCountry($cName, $capName, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `country` set `country_name` = ?, `capital` = ?) where country_id = ?");
+        $stmt->bind_param("ssi", $cName, $capName, $cid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCountry($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `country` where country_id = ?");
+        $stmt->bind_param("i", $cid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
