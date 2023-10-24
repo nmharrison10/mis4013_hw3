bb8822468ab851e191a257dc148d7decae3a5010
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-24 18:37:16 -0500

    Update model-drivers.php

--- model-drivers.php
+++ model-drivers.php
@@ -12,4 +12,45 @@ function selectDrivers() {
         throw $e;
     }
 }
+function insertDriver($tid, $cid, $dName, $dAge) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `driver` (`team_id`, `country_id`, `driver_name`, `driver_age`) VALUES (?, ?, ?, ?);");
+        $stmt->bind_param("iisi", $tid, $cid, $dName, $dAge);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateDriver($tid, $cid, $dName, $dAge, $did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `driver` set `team_id` = ?, `country_id` = ?, `driver_name`= ?, `driver_age`= ? where driver_id = ?");
+        $stmt->bind_param("iisii", $tid, $cid, $dName, $dAge, $did);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteDriver($did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `driver` where driver_id = ?");
+        $stmt->bind_param("i", $did);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
