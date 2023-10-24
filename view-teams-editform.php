074e9ec1f3128341c6ab1b84ec77a1f045131d96
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-24 17:59:11 -0500

    Update view-teams-editform.php

--- view-teams-editform.php
+++ view-teams-editform.php
@@ -23,7 +23,7 @@
           </div>
           <div class="mb-3">
             <label for="cid<?php echo $team['team_id'];?>" class="form-label">Country ID</label>
-            <input type="int" class="form-control" id="cid<?php echo $team['team_id'];?>" name="cid" value="<?php echo $team['country_id'];?>">
+            <input type="number" class="form-control" id="cid<?php echo $team['team_id'];?>" name="cid" value="<?php echo $team['country_id'];?>">
           </div>
 
           <div class="mb-3">
