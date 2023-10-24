f197fe0aabde2ec93b519f8e4c67d5b3341ec610
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-24 16:47:52 -0500

    Update countries.php

--- countries.php
+++ countries.php
@@ -9,7 +9,15 @@ if (isset($_POST['actionType'])){
 switch($_POST['actionType']){
   case "Add":
     if(insertCountry($_POST['cName'], $_POST['capName'])) {
-  echo '<div style="margin:15px" class="alert alert-success" role="alert">Course Added</div>';
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Country Added</div>';
+    }
+    else{
+echo '<div class="alert alert-danger" role="alert">Error.</div>"';
+    }
+  break;
+  case "Delete":
+    if(deleteCountry($_POST['cid'])) {
+  echo '<div style="margin:15px" class="alert alert-success" role="alert">Country Deleted/div>';
     }
     else{
 echo '<div class="alert alert-danger" role="alert">Error.</div>"';
