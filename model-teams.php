957f896b234af3b01abd00906b26886827e33dde
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-24 18:17:15 -0500

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -2,7 +2,7 @@
 function selectTeams() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT team_id, team_name, country_name, principal FROM team t join country c on t.country_id=c.country_id");
+        $stmt = $conn->prepare("SELECT team_id, team_name, t.country_id, country_name, principal FROM team t join country c on t.country_id=c.country_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
