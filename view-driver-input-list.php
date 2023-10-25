c9fee84d90f20e081aa4ee5c2761373889561349
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-24 21:42:25 -0500

    Update view-driver-input-list.php

--- view-driver-input-list.php
+++ view-driver-input-list.php
@@ -1,9 +1,9 @@
 <select class="form-select" id="did" name="did">
   <?php
 while ($driverItem = $driverList->fetch_assoc()) {
-  $selText = ""
+  $selText = "";
   if ($selectedDriver == $driverItem['driver_id']{
-      $selText = "selected"
+  $selText = " selected"
       }
   ?>
   <option value="<?php echo $driverItem['driver_id']; ?>" <?=$selText?>><?php echo $driverItem['driver_name']; ?></option>
