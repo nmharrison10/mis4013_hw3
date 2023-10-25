8590c61c484ea1dcfc5049722eeffe5ffe98efa3
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-24 21:47:43 -0500

    Update model-countries.php

--- model-countries.php
+++ model-countries.php
@@ -13,6 +13,20 @@ function selectCountries() {
     }
 }
 
+function selectCountriesForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT country_id, country_name FROM country order by country_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 function insertCountry($cName, $capName) {
     try {
         $conn = get_db_connection();
