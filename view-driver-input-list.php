8ddb22fe1b2ff280a01a6f82cc0ac4e0b94d04a6
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-10-24 21:43:25 -0500

    Update view-driver-input-list.php

--- view-driver-input-list.php
+++ view-driver-input-list.php
@@ -3,7 +3,7 @@
 while ($driverItem = $driverList->fetch_assoc()) {
   $selText = "";
   if ($selectedDriver == $driverItem['driver_id']{
-  $selText = " selected"
+  $selText = " selected";
       }
   ?>
   <option value="<?php echo $driverItem['driver_id']; ?>" <?=$selText?>><?php echo $driverItem['driver_name']; ?></option>
