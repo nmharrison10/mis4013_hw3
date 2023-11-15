7059faa000acf54abdcdc08031b997b88461e841
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-11-15 10:09:28 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="teams.php">JS Libraries</a>
+          <a class="nav-link" href="js-libraries.php">JS Libraries</a>
         <li class="nav-item">
           <a class="nav-link" href="races-chart.php">Races Chart</a>
         </li>
