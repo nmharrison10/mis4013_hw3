2e25f810c69f4280738295bb6312e785b21fb3b2
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-11-15 09:57:17 -0600

    Update view-races-chart.php

--- view-races-chart.php
+++ view-races-chart.php
@@ -1,7 +1,35 @@
 <h1>Formula 1 Races Chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-<?php
-while ($race = $races->fetch_assoc()) {
- 
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
+<?php while ($race = $races->fetch_assoc()) {
+ echo $race[`Races_Won`].",";
 }
-?
+?>
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php 
+$races = selectRaces();
+while ($race = $races->fetch_assoc()) {
+ echo "'".$race[`Races_Won`]."',";
+}
+?>
+    ]
+},
+
+  });
+</script>
